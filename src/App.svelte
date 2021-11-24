0625e1adafb5d20ad0debaf0ced9d056dc4c91c3
Abhay Zala <zala.abhay@gmail.com>
2021-11-23 23:14:52 -0500

    setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-	export let name;
+import router from 'page';
+import Index from './pages/Index.svelte';
+import Login from './pages/Login.svelte';
+import Signup from './pages/Signup.svelte';
+import Profile from './pages/Profile.svelte';
+
+let title;
+let page;
+
+router('/', () => { page = Index; title = ""; });
+
+router.start()
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<svelte:component this="{page}" />
